ae1464a1953d5bb4245db5cd8e9e44eca65a92d5
Varun Kumar <test.webdox@gmail.com>
2023-10-11 19:36:27 +0000

    allowed netlify url

--- backend/settings.py
+++ backend/settings.py
@@ -135,4 +135,5 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'https://jovial-concha-9aeade.netlify.app/'
 ]
